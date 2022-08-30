786b4db4e0388ac921e08b600182e410e8eab3ce
xiacheng <96228525+scxiacheng@users.noreply.github.com>
2022-08-30 16:05:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 using iptr = int*;
 int main()
 {
-	CClassFileParser parser("D:\\Code\\External\\ClassAnalyzer\\cache\\com\\fasterxml\\jackson\\databind\\ObjectMapper$2.class");
-	parser.Parser();
+	CClassFileParser parser("ObjectMapper$2.class");
+	parser.Parse();
 	parser.Release();
 	return 0;
 }
